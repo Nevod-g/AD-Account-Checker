5c8397b00b00e9ea111ebad553415952bb94e003
Nevod <st.matr@mail.ru>
2023-06-24 07:32:09 +0200

    Добавьте файлы проекта.

---
+++ AD Account Checker/frmMain.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits DevExpress.XtraBars.FluentDesignSystem.FluentDesignForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.FluentDesignFormContainer1 = New DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormContainer()
+        Me.AccordionControl1 = New DevExpress.XtraBars.Navigation.AccordionControl()
+        Me.AccordionControlElement1 = New DevExpress.XtraBars.Navigation.AccordionControlElement()
+        Me.FluentDesignFormControl1 = New DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormControl()
+        Me.FluentFormDefaultManager1 = New DevExpress.XtraBars.FluentDesignSystem.FluentFormDefaultManager(Me.components)
+        CType(Me.AccordionControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.FluentDesignFormControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.FluentFormDefaultManager1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'FluentDesignFormContainer1
+        '
+        Me.FluentDesignFormContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FluentDesignFormContainer1.Location = New System.Drawing.Point(260, 39)
+        Me.FluentDesignFormContainer1.Name = "FluentDesignFormContainer1"
+        Me.FluentDesignFormContainer1.Size = New System.Drawing.Size(828, 636)
+        Me.FluentDesignFormContainer1.TabIndex = 0
+        '
+        'AccordionControl1
+        '
+        Me.AccordionControl1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.AccordionControl1.Elements.AddRange(New DevExpress.XtraBars.Navigation.AccordionControlElement() {Me.AccordionControlElement1})
+        Me.AccordionControl1.Location = New System.Drawing.Point(0, 39)
+        Me.AccordionControl1.Name = "AccordionControl1"
+        Me.AccordionControl1.ScrollBarMode = DevExpress.XtraBars.Navigation.ScrollBarMode.Touch
+        Me.AccordionControl1.Size = New System.Drawing.Size(260, 636)
+        Me.AccordionControl1.TabIndex = 1
+        Me.AccordionControl1.ViewType = DevExpress.XtraBars.Navigation.AccordionControlViewType.HamburgerMenu
+        '
+        'AccordionControlElement1
+        '
+        Me.AccordionControlElement1.Name = "AccordionControlElement1"
+        Me.AccordionControlElement1.Text = "Element1"
+        '
+        'FluentDesignFormControl1
+        '
+        Me.FluentDesignFormControl1.FluentDesignForm = Me
+        Me.FluentDesignFormControl1.Location = New System.Drawing.Point(0, 0)
+        Me.FluentDesignFormControl1.Manager = Me.FluentFormDefaultManager1
+        Me.FluentDesignFormControl1.Name = "FluentDesignFormControl1"
+        Me.FluentDesignFormControl1.Size = New System.Drawing.Size(1088, 39)
+        Me.FluentDesignFormControl1.TabIndex = 2
+        Me.FluentDesignFormControl1.TabStop = False
+        '
+        'FluentFormDefaultManager1
+        '
+        Me.FluentFormDefaultManager1.Form = Me
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1088, 675)
+        Me.ControlContainer = Me.FluentDesignFormContainer1
+        Me.Controls.Add(Me.FluentDesignFormContainer1)
+        Me.Controls.Add(Me.AccordionControl1)
+        Me.Controls.Add(Me.FluentDesignFormControl1)
+        Me.FluentDesignFormControl = Me.FluentDesignFormControl1
+        Me.Name = "frmMain"
+        Me.NavigationControl = Me.AccordionControl1
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "AD Account Checker"
+        CType(Me.AccordionControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.FluentDesignFormControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.FluentFormDefaultManager1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents FluentDesignFormContainer1 As DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormContainer
+    Friend WithEvents AccordionControl1 As DevExpress.XtraBars.Navigation.AccordionControl
+    Friend WithEvents AccordionControlElement1 As DevExpress.XtraBars.Navigation.AccordionControlElement
+    Friend WithEvents FluentDesignFormControl1 As DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormControl
+    Friend WithEvents FluentFormDefaultManager1 As DevExpress.XtraBars.FluentDesignSystem.FluentFormDefaultManager
+End Class
